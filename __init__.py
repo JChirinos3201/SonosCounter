9340728d65dbcbb666a88a8e77b0e76127ce0c9b
Joan Chirinos <jchirinos3201@gmail.com>
2020-01-19 13:44:38 -0500

    testing bootstrap

--- __init__.py
+++ __init__.py
@@ -4,9 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/calendar")
 def hello():
-	return "Henlo!"
+    return '<button type="button" class="btn btn-primary">Primary</button>'
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
