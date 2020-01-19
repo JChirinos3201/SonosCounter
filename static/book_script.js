a973188181d382ff66e0cac43a35ac74277c04d7
Joan Chirinos <jchirinos3201@gmail.com>
2020-01-19 15:24:41 -0500

    trying my very best to make the JS work...

---
+++ static/book_script.js
@@ -0,0 +1,4 @@
+  var bean = function () {
+    document.getElementById('select_time').innerHTML = "<option value='' disabled selected hidden>Choose a time</option>\n<option>option 1</option>\n<option>option 2</option>\n<option>option 3</option>";
+    document.getElementById('select_time').disabled = false;
+  };
