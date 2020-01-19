e76f83ad064a690320113e6768407b479117310d
Joan Chirinos <jchirinos3201@gmail.com>
2020-01-19 15:08:01 -0500

    split head, body, scripts into pieces for compat. with bentobox

--- __init__.py
+++ __init__.py
@@ -6,9 +6,19 @@ from flask import (Flask, render_template, redirect, url_for, session, request,
 app = Flask(__name__)
 
 
-@app.route("/calendar")
-def hello():
-    return render_template('schedule_form.html')
+@app.route("/book/head")
+def get_head():
+    return render_template('book_head.html')
+
+
+@app.route("/book/body")
+def get_body():
+    return render_template('book_body.html')
+
+
+@app.route("/book/scripts")
+def get_scripts():
+    return render_template('book_scripts.html')
 
 
 if __name__ == "__main__":
